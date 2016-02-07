4d5d63f52765d9096a135358bf3923e7861c4cb7
Porama Ignoi Ruengrairatanaroj <seal.corner@gmail.com>
2016-02-07 20:55:12 +0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Database Prototype</title>
+</head>
+
+<body>
+    <h1>PHP Database Prototype</h1>
+    <?php
+		echo "<p>This is echoed PHP code.</p>"
+    ?>
+</body>
+
+</html>
